e2392a1bbc688a68fb61b5ba341c33a2ed913add
Andrew Guerrero <ajguerrer@gmail.com>
2020-12-01 20:38:57 -0700

    fermatin'

--- src/day1.rs
+++ src/day1.rs
@@ -10,6 +10,7 @@ pub fn day1_part1() -> Result<String> {
         .enumerate()
         .find_map(|(i, n1)| Some((n1, nums[i..].iter().find(|n2| n1 + *n2 == 2020)?)))
         .ok_or_else(|| anyhow!("solution not found"))?;
+
     Ok(format!("{} * {} = {}", n1, n2, n1 * n2))
 }
 
@@ -31,29 +32,29 @@ pub fn day1_part2() -> Result<String> {
 
 pub fn day1_part1_easy_mode() -> Result<String> {
     let nums = parse_file()?;
-
     let (n1, n2) = nums
         .iter()
         .tuple_combinations()
         .find(|(n1, n2)| *n1 + *n2 == 2020)
         .ok_or_else(|| anyhow!("solution not found"))?;
+
     Ok(format!("{} * {} = {}", n1, n2, n1 * n2))
 }
 
 pub fn day1_part2_easy_mode() -> Result<String> {
     let nums = parse_file()?;
-
     let (n1, n2, n3) = nums
         .iter()
         .tuple_combinations()
         .find(|(n1, n2, n3)| *n1 + *n2 + *n3 == 2020)
         .ok_or_else(|| anyhow!("solution not found"))?;
+
     Ok(format!("{} * {} * {} = {}", n1, n2, n3, n1 * n2 * n3))
 }
 
 fn parse_file() -> Result<Vec<u32>> {
     String::from_utf8_lossy(&fs::read("data/day1.txt")?)
         .lines()
-        .map(|l| Ok(l.parse()?))
+        .map(|line| Ok(line.parse()?))
         .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod day1;
 
 use anyhow::Result;
+
 fn main() -> Result<()> {
     println!("day 1, part 1: {}", day1::day1_part1()?);
     println!("day 1, part 2: {}", day1::day1_part2()?);
