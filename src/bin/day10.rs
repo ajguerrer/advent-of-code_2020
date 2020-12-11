d12cc1bf67cf7c67082533a2d4cb7c4a715affa3
Andrew Guerrero <ajguerrer@gmail.com>
2020-12-10 20:46:28 -0700

    Day 10

---
+++ src/bin/day10.rs
@@ -0,0 +1,43 @@
+use std::fs::read_to_string;
+
+fn main() {
+    println!("{}", part1());
+    println!("{}", part2());
+}
+
+fn part1() -> usize {
+    let mut adapters = parse_file();
+    adapters.sort_unstable();
+    count_joltage(&adapters, 1) * count_joltage(&adapters, 3)
+}
+
+fn count_joltage(adapters: &[u32], diff: u32) -> usize {
+    adapters
+        .iter()
+        .zip(adapters.iter().skip(1))
+        .filter(|(a1, a2)| *a2 - *a1 == diff)
+        .count()
+        + 1
+}
+
+fn part2() -> usize {
+    let mut adapters = parse_file();
+    adapters.push(0);
+    adapters.sort_unstable();
+    let mut running_total = vec![1; adapters.len()];
+    for i in 1..adapters.len() {
+        running_total[i] = (i.saturating_sub(3)..i)
+            .filter(|j| adapters[i] - adapters[*j] <= 3)
+            .map(|j| running_total[j])
+            .sum();
+    }
+    *running_total.last().unwrap()
+}
+
+fn parse_file() -> Vec<u32> {
+    read_to_string("data/day10.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect()
+}
