b7d5a8d62dc4d6319341d3558cb08fe6a55eb56d
Andrew Guerrero <ajguerrer@gmail.com>
2020-12-18 19:19:30 -0700

    Day 17 generic dimensions

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,12 +1,14 @@
-use std::{collections::HashSet, fs::read_to_string};
+#![feature(min_const_generics)]
+
+use std::{collections::HashSet, convert::TryInto, fs::read_to_string};
 
 fn main() {
-    // println!("{}", part1());
+    println!("{}", part1());
     println!("{}", part2());
 }
 
 fn part1() -> usize {
-    let mut cube = parse_file();
+    let mut cube = parse_file::<3>();
     for _ in 0..6 {
         cube = cycle(&cube);
     }
@@ -14,14 +16,14 @@ fn part1() -> usize {
 }
 
 fn part2() -> usize {
-    let mut cube = parse_file();
+    let mut cube = parse_file::<4>();
     for _ in 0..6 {
         cube = cycle(&cube);
     }
     cube.len()
 }
 
-fn cycle(cube: &HashSet<Coord>) -> HashSet<Coord> {
+fn cycle<const D: usize>(cube: &HashSet<Coord<D>>) -> HashSet<Coord<D>> {
     let neigh = cube
         .iter()
         .flat_map(|c| adjacent(c))
@@ -37,31 +39,37 @@ fn cycle(cube: &HashSet<Coord>) -> HashSet<Coord> {
         .collect()
 }
 
-fn adjacent(coord: &Coord) -> HashSet<Coord> {
-    (coord[0] - 1..=coord[0] + 1)
-        .flat_map(|x| {
-            (coord[1] - 1..=coord[1] + 1).flat_map(move |y| {
-                (coord[2] - 1..=coord[2] + 1)
-                    .flat_map(move |z| (coord[3] - 1..=coord[3] + 1).map(move |w| [x, y, z, w]))
-            })
+fn adjacent<const D: usize>(coord: &Coord<D>) -> HashSet<Coord<D>> {
+    (0..3usize.pow(D as u32))
+        .map(|i| {
+            (0..D)
+                .map(|c| coord[c] + (i / 3usize.pow(c as u32) % 3) as i32 - 1)
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap()
         })
         .collect()
 }
 
-type Coord = [i32; 4];
+type Coord<const D: usize> = [i32; D];
 
-fn parse_line((y, s): (usize, &str)) -> HashSet<Coord> {
+fn parse_line<const D: usize>((y, s): (usize, &str)) -> HashSet<Coord<D>> {
     s.chars()
         .enumerate()
         .filter_map(|(x, c)| match c {
-            '#' => Some([x as i32, y as i32, 0, 0]),
+            '#' => {
+                let mut arr = [0; D];
+                arr[0] = x as i32;
+                arr[1] = y as i32;
+                Some(arr)
+            }
             '.' => None,
             c => panic!("weird input {}", c),
         })
         .collect()
 }
 
-fn parse_file() -> HashSet<Coord> {
+fn parse_file<const D: usize>() -> HashSet<Coord<D>> {
     read_to_string("data/day17.txt")
         .unwrap()
         .lines()
